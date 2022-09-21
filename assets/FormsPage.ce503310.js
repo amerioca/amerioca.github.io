4dbec1ae2fc92edfd82f28e9fc9266a7bc1a12d7
HP17 <dgraetz@gmail.com>
2022-09-21 19:11:20 -0300

    latest

---
+++ assets/FormsPage.ce503310.js
@@ -0,0 +1,1 @@
+import{_ as e,I as r,R as c}from"./index.5077810c.js";const o={};function a(n,s){return r(),c("div",null," Forms ")}var _=e(o,[["render",a]]);export{_ as default};
---
+++ assets/recaptcha.cec61836.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.5077810c.js";var e=a(({app:o})=>{console.log("recaptcha installed")});export{e as default};
